069ab194f56a8dab3ffc4835b8025291c0a10fa0
Jace Walker <jacewalker@me.com>
2022-04-07 17:57:17 +1000

    Boxes setup

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,22 @@
   <NavbarView/>
   <ShowCaseView/>
   <NewsletterView/>
+  <BoxesView/>
 </template>
 
 <script>
 import NavbarView from './components/NavbarView.vue'
 import ShowCaseView from './components/ShowcaseView.vue'
 import NewsletterView from './components/NewsletterView.vue'
+import BoxesView from './components/BoxesView.vue'
 
 export default {
   name: 'App',
   components: {
     NavbarView,
     ShowCaseView,
-    NewsletterView
+    NewsletterView,
+    BoxesView
   }
 }
 </script>
