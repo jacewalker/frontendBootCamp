bacd182cdc8d674f23aa74c474db467eb0efb586
Jace Walker <jacewalker@me.com>
2022-04-07 17:05:10 +1000

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <FrontendBootcamp/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FrontendBootcamp from './components/FrontendBootcamp'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FrontendBootcamp,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
